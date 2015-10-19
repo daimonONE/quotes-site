f4acb5f6f59fdb0b3035ac76db8bb181f5127d7b
Dmitriy Orlov <deo@seventest.ru>
2015-10-19 20:06:33 +0300

    Trying JSON request

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,33 @@
 // JavaScript source code
 $(document).ready(function () {
 
-    // Your code here.a    
-    $(".text").text("Hello1234");    
+    var setQuote = function (quoteData) {
+
+    };
+
+
+    
+    
+    // Your code here.a
+    $("#bQuote").click(function () {
+
+        var settings =
+        {            
+            data: "method=getQuote&format=jsonp&jsonp=parseQuote",
+            success: function (data){
+                console.log(data);
+            },
+            error: function() {
+                alert('Failed');
+            },
+            type: 'GET',
+            dataType: 'jsonp'
+        };        
+        $.ajax("http://api.forismatic.com/api/1.0", settings);
+    // $.getJSON("http://api.forismatic.com/api/1.0", "", function (data) {
+            
+    //         //$(".quotes").text(data.contents.quotes[0].quote);
+    //     });
+     });
+
 });
